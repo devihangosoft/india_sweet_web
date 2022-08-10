9bfa5acf174aad321cca3a4af95c2ea8c4ab6b11
unknown <poojasingh.bisht@hagnosoft.com>
2022-08-10 13:50:58 +0530

    Dashboard

---
+++ src/components/Elements/Charts/LinearProgressBar/index.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Linear from './Linear'
+export default function LinearProgressBar() {
+  return (
+    <>
+    <h5 style={{fontWeight:400}}>Most Sold Sweets</h5><br/>
+   <Linear/>
+   <Linear/>
+   <Linear/>
+   <Linear/>
+   <Linear/>
+   <Linear/>
+   
+   </>
+  );
+}
