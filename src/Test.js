6e93f38b606c62d1caf9ee4190e897c7493e9ab4
devisinghlodhi <39167328+devisinghlodhi@users.noreply.github.com>
2022-08-24 12:09:18 +0530

    useAxios error fix get request 401

--- src/Test.js
+++ src/Test.js
@@ -1,44 +1,77 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-// const axios = require('axios');
+import useAxios from './components/hooks/useAxios';
 
 const Test = () => {
+
+    const userData = JSON.parse(localStorage.getItem("user"));
+    const [apiState, setapiState] = useState(1);
+    const { response, loading, error } = useAxios({
+      method: "post",
+      url: "/test",
+      body: JSON.stringify({
+        user_id: `${userData.data.data[0].user_id}`,
+      }),
+      apiState: apiState,
+    });
+
+    useEffect(() => {
+      if (response !== null) {
+       console.log(response);
+        // setSuccessmessage(response.message);
+      }
+  
+      const resMessage =
+        (error.response && error.response.data && error.response.data.message) ||
+        error.message ||
+        error.toString();
+      // setMessage(resMessage);
+      console.log(resMessage)
+      setTimeout(() => {
+        // setMessage("");
+      }, 5000);
+    }, [response, error]);
+
+
+
     
-    const apicall = async()=>{
+    // const apicall = async()=>{
       
-        let headersList = {
-            "Accept": "*/*",
-            "Content-Type": "application/json",
-            "api-key": "F2PE47UiPImpXj5bEJQymSynK9wAYSd71Lon1haWXLYRGfbRS6K51dQ_NQ",
-            // "Authorization": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY2MDYzMzc4MiwianRpIjoiODM5ZGQ3OWEtOTYyOC00NDU3LWJkZDEtOTE5NDAwNjcwNzcwIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IkRldkAxMjM0IiwibmJmIjoxNjYwNjMzNzgyLCJleHAiOjE2NjA2NTUzODJ9.YN6CklmVTyUla7FStH5PmWQxOfzoyMnYsbice_gY65o" 
-            "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY2MTI1Njk4MiwianRpIjoiYmQ0MjQxOTctZWNlMC00YjBkLThlNzUtMzE0ZjdhYmE3MGM1IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6Im5ldyIsIm5iZiI6MTY2MTI1Njk4MiwiZXhwIjoxNjYxMjc4NTgyfQ.QSW3Iu992X3wKjv2uQCrzstKm0kJlNDDQmsEYpdiieY" 
-           }
+    //     let headersList = {
+    //         "Accept": "*/*",
+    //         "Content-Type": "application/json",
+    //         "api-key": "F2PE47UiPImpXj5bEJQymSynK9wAYSd71Lon1haWXLYRGfbRS6K51dQ_NQ",
+    //         // "Authorization": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY2MDYzMzc4MiwianRpIjoiODM5ZGQ3OWEtOTYyOC00NDU3LWJkZDEtOTE5NDAwNjcwNzcwIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IkRldkAxMjM0IiwibmJmIjoxNjYwNjMzNzgyLCJleHAiOjE2NjA2NTUzODJ9.YN6CklmVTyUla7FStH5PmWQxOfzoyMnYsbice_gY65o" 
+    //         "Authorization": "Bearer "+"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY2MTMxODEwOCwianRpIjoiZTFmYWZmZjAtMjdkMS00ODM2LWI4MDQtNGI4ZGZkOGRiZjA5IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6Im5ldyIsIm5iZiI6MTY2MTMxODEwOCwiZXhwIjoxNjYxMzM5NzA4fQ.hMRrku_-TxJ5cYVt9ALlL7mU7WfXdd1ezfhhN1sOJUo" 
+    //        }
            
-           let bodyContent = JSON.stringify({
-             "user_id": "8740618d-a140-4a96-9999-32212a4926fb",
-             "product_name": "DOODHA BURFI",
-             "product_details": "indian sweet house ki mithaai ",
-             "quantity": "1kg",
-             "price": "700"
-           });
+    //        let bodyContent = JSON.stringify({
+    //          "user_id": "8740618d-a140-4a96-9999-32212a4926fb",
+    //          "product_name": "DOODHA BURFI",
+    //          "product_details": "indian sweet house ki mithaai ",
+    //          "quantity": "1kg",
+    //          "price": "700"
+    //        });
            
+    //        let reqOptions = {
+    //         //  url: "http://216.48.182.12:5000/test",
+    //          url: "http://192.168.8.101:5000/test",             
+    //          method: "GET",
+    //          headers: headersList,
+    //          data: bodyContent,
+    //        }
+           
+    //        let response = await axios.request(reqOptions);
+    //        console.log(response.data);
 
-           
-           let reqOptions = {
-             url: "http://216.48.182.12:5000/test",
-            //  url: "http://192.168.8.101:5000/test",             
-             method: "POST",
-             headers: headersList,
-            //  data: bodyContent,
-           }
-           
-           let response = await axios.request(reqOptions);
-           console.log(response.data);
-
-    }
+    // }
 
     
-    apicall();
+    // apicall();
+
+
+
+
 
 
     return (
